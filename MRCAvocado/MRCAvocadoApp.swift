09b165e4edbca2681e330bc9c37ba6ca0d22ad8d
Anastasiia Tereshchenko <ms.staisy@gmail.com>
2022-08-20 21:10:27 -0300

    Initial Commit

---
+++ MRCAvocado/MRCAvocadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MRCAvocadoApp.swift
+//  MRCAvocado
+//
+//  Created by Onie on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MRCAvocadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
