1ca1a15295500705fb254bc4f8b923d4c4f9dc3c
Anastasiia Tereshchenko <ms.staisy@gmail.com>
2022-08-28 11:53:55 -0700

    0019: Assignment 6

--- MCRAvocado/MyPlayground.playground/Contents.swift
+++ MCRAvocado/MyPlayground.playground/Contents.swift
@@ -1,9 +1,9 @@
 import UIKit
 
-// MARK: - Assigment 1
+// MARK: - Assignment  1
 
 /// category of meal
-enum MealType {
+enum MenuItemType {
 	case scramble
 	case omelets
 	case eggsBenedict
@@ -13,12 +13,6 @@ enum MealType {
 	case bagel
 	case dessert
 	case side
-	case other
-}
-
-
-/// category of beverage
-enum BeverageType {
 	case tea
 	case coffee
 	case juice
@@ -48,80 +42,58 @@ enum CupSize {
 	case venti
 }
 
-
-/// Protocol for items in Menu
-protocol MenuItem {
-	var name: String { get }
-	var price: Double { get }
-	var isInStock: Bool { get }
-	var calories: Int { get }
-	var description: String? { get }
-}
-
-/// Meal
-struct Meal: MenuItem {
-	let name: String
-	let price: Double
-	let isInStock: Bool
-	let calories: Int
-	let description: String?
-	let type: MealType
-}
-
-extension Meal: Hashable {
-	static func == (lhs: Meal, rhs: Meal) -> Bool {
-		return lhs.name == rhs.name && lhs.name == rhs.name
-	}
-}
-
-/// Beverage
-struct Beverage: MenuItem {
+/// MenuItem
+struct MenuItem {
 	let name: String
 	let price: Double
 	var isInStock: Bool
 	let calories: Int
 	let description: String?
-	let type: BeverageType
-	let withIce: Bool? // Assigment 3
-	let typeOfMilk: MilkType?  // Assigment 3
-	let cupSize: CupSize
+	let type: MenuItemType
+	let withIce: Bool? // Assignment  3
+	let typeOfMilk: MilkType? // Assignment  3
+	let cupSize: CupSize?
+	
+	init(name: String, price: Double, isInStock: Bool, calories: Int, description: String? = nil, type: MenuItemType, withIce: Bool? = nil , typeOfMilk: MilkType? = nil, cupSize: CupSize? = nil) {
+		self.name = name
+		self.price = price
+		self.isInStock = isInStock
+		self.calories = calories
+		self.description = description
+		self.type = type
+		self.withIce = withIce
+		self.typeOfMilk = typeOfMilk
+		self.cupSize = cupSize
+	}
 }
 
-extension Beverage: Hashable {
-	static func == (lhs: Beverage, rhs: Beverage) -> Bool {
+extension MenuItem: Hashable {
+	static func == (lhs: MenuItem, rhs: MenuItem) -> Bool {
 		return lhs.name == rhs.name && lhs.name == rhs.name
 	}
 }
 
-// MARK: - Assigment 2
-
-// I whant to create  dictionary like that:
-// let order: [MenuItem: Int] = [:]
-// But protocol can't be a key value
-// That why I create  2 dictionaries and 1 struct instead
-// I hope it's ok
-let mealOrder: [Meal: Int] = [:]
-var beverageOrder: [Beverage: Int] = [:]
-
-
 /// Order
 struct Order {
-	var mealOrder: [Meal: Int]
-	var beverageOrder: [Beverage: Int]
+	var orderedItems: [MenuItem: Int]
 	let numberOfPersons: Int
 	let tableNumber: Int
 	let dateOfCreation: Date
 	
 	var totalPrice: Double {
 		var totalPrice = 0.0
-		for a in mealOrder {
+		for a in orderedItems {
 			totalPrice += a.key.price
 		}
 		return totalPrice
 	}
 }
 
-// MARK: - Assigment 4
+// MARK: - Assignment  2
+
+let orderedItems: [MenuItem: Int] = [:]
+
+// MARK: - Assignment  4
 
 enum DescriptionText {
 	public static let butterWaffle = "Our house-made Belgian Waffle topped with whipped real butter"
@@ -130,28 +102,28 @@ enum DescriptionText {
 	public static let icedCoffee = "Handcrafted, iced cold brew coffee made from 100% Arabica beans. Comes with milk for you to customize as you like."
 }
 
-let butterWaffle = Meal(name: "Waffle with butter",
-						price: 12.79,
-						isInStock: false,
-						calories: 570,
-						description: DescriptionText.butterWaffle,
-						type: .waffle)
+let butterWaffle = MenuItem(name: "Waffle with butter",
+							price: 12.79,
+							isInStock: false,
+							calories: 570,
+							description: DescriptionText.butterWaffle,
+							type: .waffle)
 
-let eggsBaconWaffle = Meal(name: "Waffle with eggs and bacon",
-						   price: 14.99,
-						   isInStock: true,
-						   calories: 1000,
-						   description: DescriptionText.eggsBaconWaffle,
-						   type: .waffle)
+let eggsBaconWaffle = MenuItem(name: "Waffle with eggs and bacon",
+							   price: 14.99,
+							   isInStock: true,
+							   calories: 1000,
+							   description: DescriptionText.eggsBaconWaffle,
+							   type: .waffle)
 
-let blueberryPancake = Meal(name: "Blueberry pancake",
-							price: 11.99,
-							isInStock: true,
-							calories: 610,
-							description: DescriptionText.blueberryPancake,
-							type: .pancake)
+let blueberryPancake = MenuItem(name: "Blueberry pancake",
+								price: 11.99,
+								isInStock: true,
+								calories: 610,
+								description: DescriptionText.blueberryPancake,
+								type: .pancake)
 
-let blackTea = Beverage(name: "Black tea",
+let blackTea = MenuItem(name: "Black tea",
 						price: 2.99,
 						isInStock: true,
 						calories: 0,
@@ -162,17 +134,17 @@ let blackTea = Beverage(name: "Black tea",
 						cupSize: .tall)
 
 
-let icedCoffee = Beverage(name: "Iced cofee",
+let icedCoffee = MenuItem(name: "Iced cofee",
 						  price: 5.99,
 						  isInStock: true,
-						  calories: 0,
+						  calories: 20,
 						  description: DescriptionText.icedCoffee,
 						  type: .coffee,
 						  withIce: true,
 						  typeOfMilk: .coconut,
 						  cupSize: .tall)
 
-let orangeJuice = Beverage(name: "orangeJuice",
+let orangeJuice = MenuItem(name: "orangeJuice",
 						   price: 4.99,
 						   isInStock: true,
 						   calories: 150,
@@ -182,9 +154,8 @@ let orangeJuice = Beverage(name: "orangeJuice",
 						   typeOfMilk: nil,
 						   cupSize: .grande)
 
-// MARK: - Assigment 5
-var order = Order(mealOrder: [eggsBaconWaffle: 1, blueberryPancake: 1],
-				  beverageOrder: [icedCoffee:1, orangeJuice: 1],
+// MARK: - Assignment  5
+var order = Order(orderedItems: [eggsBaconWaffle: 1, blueberryPancake: 1,icedCoffee:1, orangeJuice: 1],
 				  numberOfPersons: 2,
 				  tableNumber: 28,
 				  dateOfCreation: Date())
@@ -194,15 +165,46 @@ func addMenuItem(item: MenuItem, amount: Int, to order: inout Order) {
 		print("Sorry! Out of Stock :(")
 		return
 	}
-	if let beverage = item as? Beverage {
-		let existingAmount: Int = order.beverageOrder[beverage] ?? 0
-		order.beverageOrder[beverage] = existingAmount + amount
-	} else if let meal = item as? Meal {
-		let existingAmount: Int = order.mealOrder[meal] ?? 0
-		order.mealOrder[meal] = existingAmount + amount
-	}
+	let existingAmount: Int = order.orderedItems[item] ?? 0
+	order.orderedItems[item] = existingAmount + amount
 }
 
 addMenuItem(item: butterWaffle, amount: 2, to: &order)
 addMenuItem(item: orangeJuice, amount: 2, to: &order)
 
+// MARK: - Assignment  6
+
+func createStringForPrint(from menuItem: MenuItem) -> String {
+	var result = "Name: \(menuItem.name), Price: \(menuItem.price), Is in stock: \(menuItem.isInStock), Calories: \(menuItem.calories), Type: \(menuItem.type)"
+	if let description = menuItem.description {
+		result += ", Description: \(description)"
+	}
+	if let typeOfMilk = menuItem.typeOfMilk {
+		result += ", Type of milk: \(typeOfMilk)"
+	}
+	if let withIce = menuItem.withIce {
+		result += ", With ice: \(withIce)"
+	}
+	if let cupSize = menuItem.cupSize {
+		result += ", Cup Sise: \(cupSize)"
+	}
+	result += ", Type: \(menuItem.type)"
+	
+	return result
+}
+
+func printOrderedItems(from order: Order) {
+	for (item, amount) in order.orderedItems {
+		print("\(createStringForPrint(from: item)) : \(amount)")
+	}
+}
+printOrderedItems(from: order)
+
+
+func printOrderedItemsNameAngAmount(from order: Order) {
+	for (item, amount) in order.orderedItems {
+		print("\(item.name) : \(amount)")
+	}
+}
+
+printOrderedItemsNameAngAmount(from: order)
